86cb46594886d62b05ed4402f40f5328e9225bb5
Antoine THEBAUD (contractor) <antoine.thebaud@yahoo.fr>
2019-06-20 18:34:28 +0200

    REFACTOR : moved gear-related unit tests to a new gear.js file

---
+++ test/gear.js
@@ -0,0 +1,73 @@
+
+QUnit.module("gear-unit-tests", {
+    beforeEach: function(assert) {
+        renderSlots();
+        renderSummary();
+        renderButtonbar(); // remove need of button bar in this unit test (= save anima allocation as attribute to gear maybe ?)
+        initiateButtonBar();
+        initiateSelectHandlers();
+        initiateSummary();
+        createTankBuild();
+    }
+});
+
+//TODO/TEST : to test all the functions of swlcalc-gear
+
+QUnit.test("gear should contain 9 slots", function(assert) {
+    assert.equal(swlcalc.gear.nbSlots(), 9);
+});
+
+QUnit.test("should collect all slot states", function(assert) {
+    var slotStates = swlcalc.gear.state();
+
+    assert.deepEqual(slotStates.weapon.equipment_rarity, "epic");
+    assert.deepEqual(slotStates.weapon.equipment_quality, "1");
+    assert.deepEqual(slotStates.weapon.equipment_level, "30");
+    assert.deepEqual(slotStates.weapon.glyph_id, "critical-rating");
+    assert.deepEqual(slotStates.weapon.glyph_rarity, "superior");
+    assert.deepEqual(slotStates.weapon.glyph_quality, "1");
+    assert.deepEqual(slotStates.weapon.glyph_level, "1");
+    assert.deepEqual(slotStates.weapon.signet_id, "6");
+    assert.deepEqual(slotStates.weapon.signet_rarity, undefined);
+    assert.deepEqual(slotStates.weapon.signet_level, undefined);
+
+    assert.deepEqual(slotStates.head.equipment_rarity, "epic");
+    assert.deepEqual(slotStates.head.equipment_quality, "1");
+    assert.deepEqual(slotStates.head.equipment_level, "30");
+    assert.deepEqual(slotStates.head.glyph_id, "hit-rating");
+    assert.deepEqual(slotStates.head.glyph_rarity, "mythic");
+    assert.deepEqual(slotStates.head.glyph_quality, "3");
+    assert.deepEqual(slotStates.head.glyph_level, "1");
+    assert.deepEqual(slotStates.head.signet_id, "20");
+    assert.deepEqual(slotStates.head.signet_rarity, "superior");
+    assert.deepEqual(slotStates.head.signet_level, "15");
+});
+
+//TODO/REFACTOR : to harmnonize numerical values and strings (to have only one type ?)
+QUnit.test("should collect all mapped slot states", function(assert) {
+    var slotStates = swlcalc.gear.mappedState();
+
+    assert.deepEqual(slotStates.weapon.equipment_id, "1");
+    assert.deepEqual(slotStates.weapon.equipment_rarity, 3);
+    assert.deepEqual(slotStates.weapon.equipment_quality, "1");
+    assert.deepEqual(slotStates.weapon.equipment_level, "30");
+    assert.deepEqual(slotStates.weapon.glyph_id, 2);
+    assert.deepEqual(slotStates.weapon.glyph_rarity, 2);
+    assert.deepEqual(slotStates.weapon.glyph_quality, "1");
+    assert.deepEqual(slotStates.weapon.glyph_level, "1");
+    assert.deepEqual(slotStates.weapon.signet_id, "6");
+    assert.deepEqual(slotStates.weapon.signet_rarity, 0);
+    assert.deepEqual(slotStates.weapon.signet_level, 0);
+
+    assert.deepEqual(slotStates.head.equipment_id, "1");
+    assert.deepEqual(slotStates.head.equipment_rarity, 3);
+    assert.deepEqual(slotStates.head.equipment_quality, "1");
+    assert.deepEqual(slotStates.head.equipment_level, "30");
+    assert.deepEqual(slotStates.head.glyph_id, 1);
+    assert.deepEqual(slotStates.head.glyph_rarity, 4);
+    assert.deepEqual(slotStates.head.glyph_quality, "3");
+    assert.deepEqual(slotStates.head.glyph_level, "1");
+    assert.deepEqual(slotStates.head.signet_id, "20");
+    assert.deepEqual(slotStates.head.signet_rarity, 2);
+    assert.deepEqual(slotStates.head.signet_level, "15");
+});
--- test/slots.js
+++ test/slots.js
@@ -252,10 +252,6 @@ QUnit.test("should get and set the slot's signet description", function(assert) 
 
 //TODO/TEST test('should set talisman glyph ilvl', 1, function() {
 
-QUnit.test("slots should have length of 9", function(assert) {
-    assert.equal(swlcalc.gear.nbSlots(), 9);
-});
-
 QUnit.test("should reset slot state", function(assert) {
     swlcalc.gear.slots.head.reset();
 
@@ -302,59 +298,4 @@ QUnit.test("should collect current mapped slot state", function(assert) {
     assert.deepEqual(slotState.signet_id, "20");
     assert.deepEqual(slotState.signet_rarity, 2);
     assert.deepEqual(slotState.signet_level, "15");
-});
-
-QUnit.test("should collect all slot states", function(assert) {
-    var slotStates = swlcalc.gear.state();
-
-    assert.deepEqual(slotStates.weapon.equipment_rarity, "epic");
-    assert.deepEqual(slotStates.weapon.equipment_quality, "1");
-    assert.deepEqual(slotStates.weapon.equipment_level, "30");
-    assert.deepEqual(slotStates.weapon.glyph_id, "critical-rating");
-    assert.deepEqual(slotStates.weapon.glyph_rarity, "superior");
-    assert.deepEqual(slotStates.weapon.glyph_quality, "1");
-    assert.deepEqual(slotStates.weapon.glyph_level, "1");
-    assert.deepEqual(slotStates.weapon.signet_id, "6");
-    assert.deepEqual(slotStates.weapon.signet_rarity, undefined);
-    assert.deepEqual(slotStates.weapon.signet_level, undefined);
-
-    assert.deepEqual(slotStates.head.equipment_rarity, "epic");
-    assert.deepEqual(slotStates.head.equipment_quality, "1");
-    assert.deepEqual(slotStates.head.equipment_level, "30");
-    assert.deepEqual(slotStates.head.glyph_id, "hit-rating");
-    assert.deepEqual(slotStates.head.glyph_rarity, "mythic");
-    assert.deepEqual(slotStates.head.glyph_quality, "3");
-    assert.deepEqual(slotStates.head.glyph_level, "1");
-    assert.deepEqual(slotStates.head.signet_id, "20");
-    assert.deepEqual(slotStates.head.signet_rarity, "superior");
-    assert.deepEqual(slotStates.head.signet_level, "15");
-});
-
-//TODO/REFACTOR : to have only numerical values, not strings
-QUnit.test("should collect all mapped slot states", function(assert) {
-    var slotStates = swlcalc.gear.mappedState();
-
-    assert.deepEqual(slotStates.weapon.equipment_id, "1");
-    assert.deepEqual(slotStates.weapon.equipment_rarity, 3);
-    assert.deepEqual(slotStates.weapon.equipment_quality, "1");
-    assert.deepEqual(slotStates.weapon.equipment_level, "30");
-    assert.deepEqual(slotStates.weapon.glyph_id, 2);
-    assert.deepEqual(slotStates.weapon.glyph_rarity, 2);
-    assert.deepEqual(slotStates.weapon.glyph_quality, "1");
-    assert.deepEqual(slotStates.weapon.glyph_level, "1");
-    assert.deepEqual(slotStates.weapon.signet_id, "6");
-    assert.deepEqual(slotStates.weapon.signet_rarity, 0);
-    assert.deepEqual(slotStates.weapon.signet_level, 0);
-
-    assert.deepEqual(slotStates.head.equipment_id, "1");
-    assert.deepEqual(slotStates.head.equipment_rarity, 3);
-    assert.deepEqual(slotStates.head.equipment_quality, "1");
-    assert.deepEqual(slotStates.head.equipment_level, "30");
-    assert.deepEqual(slotStates.head.glyph_id, 1);
-    assert.deepEqual(slotStates.head.glyph_rarity, 4);
-    assert.deepEqual(slotStates.head.glyph_quality, "3");
-    assert.deepEqual(slotStates.head.glyph_level, "1");
-    assert.deepEqual(slotStates.head.signet_id, "20");
-    assert.deepEqual(slotStates.head.signet_rarity, 2);
-    assert.deepEqual(slotStates.head.signet_level, "15");
 });
