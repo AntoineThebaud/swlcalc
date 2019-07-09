deee6384c58025ce1ce3ae5bcaebf9657df23726
Antoine THEBAUD (contractor) <antoine.thebaud@yahoo.fr>
2019-07-09 18:50:45 +0200

    TEST : improved tests for utility functions

--- test/util.js
+++ test/util.js
@@ -8,6 +8,10 @@ QUnit.test("should capitalize word", function(assert) {
 QUnit.test("should return blank if None/none", function(assert) {
     assert.deepEqual(swlcalc.util.blankIfNone("none"), "");
     assert.deepEqual(swlcalc.util.blankIfNone("None"), "");
+    assert.deepEqual(swlcalc.util.blankIfNone("Unchanged"), "Unchanged");
 });
 
-//TODO/TEST : add test for precisionRound()
+QUnit.test("should return correctly-rounded value", function(assert) {
+    assert.equal(swlcalc.util.precisionRound(48.1234, 3), 48.123);
+    assert.equal(swlcalc.util.precisionRound(48.12345, 4), 48.1235);
+});
