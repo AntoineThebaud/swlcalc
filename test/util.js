aca494d7fbba22c43ab0deb07de873139d9f6ad8
AntoineThebaud <antoine.thebaud@yahoo.fr>
2020-09-28 08:44:13 +0200

    TEST : utils : added new tests to cover isPrimaryStat() and isSecondaryStat()

--- test/util.js
+++ test/util.js
@@ -15,3 +15,13 @@ QUnit.test("should return correctly-rounded value", function(assert) {
     assert.equal(swlcalc.util.precisionRound(48.1234, 3), 48.123);
     assert.equal(swlcalc.util.precisionRound(48.12345, 4), 48.1235);
 });
+
+QUnit.test("should identify a primary stat", function(assert) {
+    assert.equal(swlcalc.util.isPrimaryStat('hitpoints'), true);
+    assert.equal(swlcalc.util.isPrimaryStat('critical-rating'), false);
+});
+
+QUnit.test("should identify a secondary stat", function(assert) {
+    assert.equal(swlcalc.util.isSecondaryStat('hitpoints'), false);
+    assert.equal(swlcalc.util.isSecondaryStat('critical-rating'), true);
+});
