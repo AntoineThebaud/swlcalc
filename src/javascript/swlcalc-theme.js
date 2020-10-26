57d3ae7c1d081cd17907c135d1c76905f382ed69
AntoineThebaud <antoine.thebaud@yahoo.fr>
2020-10-26 08:40:05 +0100

    Fixed some indents

--- src/javascript/swlcalc-theme.js
+++ src/javascript/swlcalc-theme.js
@@ -1,50 +1,50 @@
 var swlcalc = swlcalc || {};
 
 /**
- * Defines logic for theme switch
- */
+* Defines logic for theme switch
+*/
 swlcalc.themeSwitch = function () {
 
-      var el = {};
+    var el = {};
 
-      var elInit = function() {
-          return {
-              btn_theme_classic: $('#btn-theme-classic'),
-              btn_theme_sw:      $('#btn-theme-sw')
-          };
-      };
+    var elInit = function() {
+        return {
+            btn_theme_classic: $('#btn-theme-classic'),
+            btn_theme_sw:      $('#btn-theme-sw')
+        };
+    };
 
-      var init = function() {
-          el = elInit();
-          bindEvents();
-      };
+    var init = function() {
+        el = elInit();
+        bindEvents();
+    };
 
-      /**
-       * Associates the right processing to each trigger.
-       */
-      var bindEvents = function() {
-          el.btn_theme_classic.on('click', switchTheme);
-          el.btn_theme_sw.on('click', switchTheme);
-      };
+    /**
+    * Associates the right processing to each trigger.
+    */
+    var bindEvents = function() {
+        el.btn_theme_classic.on('click', switchTheme);
+        el.btn_theme_sw.on('click', switchTheme);
+    };
 
-      /**
-       * Switch theme to the one selected.
-       */
-      var switchTheme = function(event) {
-          var newTheme = event.currentTarget.id.split('-')[2]; //TODO better way to retrieve this value maybe ?
+    /**
+    * Switch theme to the one selected.
+    */
+    var switchTheme = function(event) {
+        var newTheme = event.currentTarget.id.split('-')[2]; //TODO better way to retrieve this value maybe ?
 
-          var htmlTag = $('html')[0];
-          htmlTag.setAttribute('theme', newTheme);
+        var htmlTag = $('html')[0];
+        htmlTag.setAttribute('theme', newTheme);
 
-          var newNavTheme = (newTheme == "classic" ? "navbar-default" : "navbar-inverse")
-          var navTag = $('nav')[0];
-          navTag.className = navTag.className.replace(/navbar-(inverse|default)/g, newNavTheme);
-      };
+        var newNavTheme = (newTheme == "classic" ? "navbar-default" : "navbar-inverse")
+        var navTag = $('nav')[0];
+        navTag.className = navTag.className.replace(/navbar-(inverse|default)/g, newNavTheme);
+    };
 
-      var oPublic = {
-          el: el,
-          init: init
-      };
+    var oPublic = {
+        el: el,
+        init: init
+    };
 
-      return oPublic;
+    return oPublic;
 }();
