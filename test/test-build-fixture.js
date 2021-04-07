e5901d3180448728f6d14230a2c5fa69c7156156
AntoineThebaud <antoine.thebaud@yahoo.fr>
2021-04-07 09:25:58 +0200

    TEST : Tests revamp [PART 5] : reworked application tests - now using createShuffledBuild instead of custom hash

--- test/test-build-fixture.js
+++ test/test-build-fixture.js
@@ -227,7 +227,7 @@ function createTankBuild() {
 
 /**
  * Create a shuffled build
- * -> equivalent to : #weapon=5,119,3,19,2,2,1,20,0,2,0&weapon2=4,27,2,28,3,4,4,17,0,8,0&head=1,3,1,4,2,1,3,11,3,10,15&finger=5,2,3,69,4,2,1,11,3,1,7&neck=4,5,3,35,1,0,1,12,1,0,9&wrist=1,0,1,20,3,3,4,20,1,0,20&luck=1,0,1,20,1,0,1,20,5,8,20&waist=2,13,3,1,2,3,2,1,2,1,1&occult=5,4,4,70,4,0,1,20,3,2,13&agent1=15,50&agent2=37,25&agent3=10,50&aa=38,5,57&passives=1400,880,960,388,916,2997,488,477,634,2154,494,753,520,270,1950,7.2,4.2,28.8,15.6
+ * -> equivalent to : #weapon=5,119,3,19,2,2,1,20,0,2,0&weapon2=4,27,2,28,3,4,4,17,0,8,0&head=1,3,1,4,2,1,3,11,3,10,15&finger=5,2,3,69,4,2,1,11,3,1,7&neck=4,5,3,35,1,0,1,1,1,0,1&wrist=1,0,1,1,3,3,4,20,1,0,1&luck=1,0,1,1,1,0,1,1,5,8,20&waist=2,13,3,1,2,3,2,1,2,1,1&occult=5,4,4,70,1,0,1,1,3,2,13&agent1=15,50&agent2=37,25&agent3=10,50&aa=38,5,57&passives=1400,880,960,388,916,2997,488,477,634,2154,494,753,520,270,1950,7.2,4.2,28.8,16.8
  */
 function createShuffledBuild() {
     $("#weapon-edit-equipment-id").val("119").change();
@@ -237,7 +237,7 @@ function createShuffledBuild() {
     $("#weapon-edit-glyph-id").val("critical-rating").change();
     $("#weapon-edit-glyph-rarity").val("superior").change();
     $("#weapon-edit-glyph-quality").val("1").change();
-    $("#weapon-edit-glyph-level").val("1").change();
+    $("#weapon-edit-glyph-level").val("20").change();
     $("#weapon-edit-signet-id").val("2").change();
 
     $("#weapon2-edit-equipment-id").val("27").change();
