845b8d5ebcab64238b7abc33084bf2d4ee2f13d7
AntoineThebaud <antoine.thebaud@yahoo.fr>
2021-04-11 17:29:10 +0200

    TEST : Tests revamp [PART 20] : Improved coverage of cascaded updates in AA tests

--- test/anima-allocation.js
+++ test/anima-allocation.js
@@ -52,7 +52,6 @@ QUnit.module("anima-allocation-integration-tests", {
     }
 });
 
-// TODO : cover equipment's transformed values, signet bonuses, agents etc
 QUnit.test("should set anima allocation damage and do updates accordingly", function(assert) {
     swlcalc.animaAllocation.setDamagePercentage("33");
 
@@ -60,10 +59,20 @@ QUnit.test("should set anima allocation damage and do updates accordingly", func
     assert.equal(swlcalc.animaAllocation.getDamageRatio(), "0.33");
     assert.equal($("#stat-combat-power").html(), "584.6");
     assert.equal($("#stat-attack-rating").html(), "4056");
+    assert.equal(swlcalc.gear.slots.neck.recap.el.equipmentDescription.text(), "Whenever you hit an enemy who is below 50% of their maximum health, you apply a damage over time effect that deals 88 physical damage over 5 seconds. Your damage is increased by 4.5% for each enemy affected by this effect.");
+    assert.equal(swlcalc.gear.slots.weapon.recap.el.signetDescription.text(), "This weapon deals 625 additional damage to enemies that are below 35% health.");
     assert.equal($("#stat-agent3-bonus50").text(), '126 Damage on Critical Hits');
+    // signet bonuses
+
+    swlcalc.animaAllocation.setDamagePercentage("0");
+    for (var slotId in swlcalc.gear.slots) {
+        var slot = swlcalc.gear.slots[slotId];
+        if(!slot.isWeapon()) {
+            assert.ok(slot.recap.el.equipmentStatARSection.is(":hidden"));
+        }
+    }
 });
 
-// TODO : cover equipment's transformed values, signet bonuses, agents etc
 QUnit.test("should set anima allocation damage and do updates accordingly", function(assert) {
     swlcalc.animaAllocation.setHealingPercentage("22");
 
@@ -71,16 +80,31 @@ QUnit.test("should set anima allocation damage and do updates accordingly", func
     assert.equal(swlcalc.animaAllocation.getHealingRatio(), "0.22");
     assert.equal($("#stat-healing-power").html(), "141.2");
     assert.equal($("#stat-heal-rating").html(), "3323");
+    assert.equal(swlcalc.gear.slots.waist.recap.el.equipmentDescription.text(), "Reduces the amount of self-damage dealt by Corruption and Martyrdom by 10%. In addition, once you have lost 10% of your health due to corruption or martyrdom, your next damaging Blood Magic ability will deal an additional 257 magical damage or your next healing Blood Magic ability will restore an additional 60 health.");
+    assert.equal(swlcalc.gear.slots.weapon2.recap.el.equipmentDescription.text(), "Whenever you heal yourself or an ally, you have a 10% chance to apply a barrier which absorbs 100% of incoming damage and dissipates after absorbing 529 damage. If a barrier already exists, they are instead healed for 529. This effect can only occur once every 5 seconds.");
     assert.equal($("#stat-agent1-bonus50").text(), '30 Healing on Critical Heals');
+
+    swlcalc.animaAllocation.setHealingPercentage("0");
+    for (var slotId in swlcalc.gear.slots) {
+        var slot = swlcalc.gear.slots[slotId];
+        if(!slot.isWeapon()) {
+            assert.ok(slot.recap.el.equipmentStatHRSection.is(":hidden"));
+        }
+    }
 });
 
-// TODO : cover equipment's transformed values, signet bonuses, agents etc
 QUnit.test("should set anima allocation damage and do updates accordingly", function(assert) {
     swlcalc.animaAllocation.setSurvivabilityPercentage("11");
 
     assert.equal(swlcalc.animaAllocation.getSurvivabilityPercentage(), "11");
     assert.equal(swlcalc.animaAllocation.getSurvivabilityRatio(), "0.11");
     assert.equal($("#stat-hit-points").html(), "8061");
-});
 
-//TODO : add test to check that e.g equipmentStatHRSection is hidden when survivability AA is set to 0
+    swlcalc.animaAllocation.setSurvivabilityPercentage("0");
+    for (var slotId in swlcalc.gear.slots) {
+        var slot = swlcalc.gear.slots[slotId];
+        if(!slot.isWeapon()) {
+            assert.ok(slot.recap.el.equipmentStatHPSection.is(":hidden"));
+        }
+    }
+});
