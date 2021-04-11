1cd65ea53fdbbe969854c3b69be3cfd44704f086
AntoineThebaud <antoine.thebaud@yahoo.fr>
2021-04-11 13:56:33 +0200

    TEST : Tests revamp [PART 16] : reworked swap tests - better coverage of summary updates

--- test/swap.js
+++ test/swap.js
@@ -1,22 +1,14 @@
 
-QUnit.module("swap-unit-tests", {
+QUnit.module("swap-integration-tests", {
     beforeEach: function(assert) {
-        renderGearOLD();
-        renderPassives();
-        renderAnimaAllocation();
-        renderSummary();
-        initiateGearHandlers();
-        initiateSummary();
-        initiateAnimaAllocation();
-        initiatePassives();
+        includeGear();
+        includeAnimaAllocation();
+        includeSummary();
+        includePassives();
         initiateSwapHandlers();
-        createTankBuild();
-    }
-});
 
-QUnit.test("should have primary weapon drawn and secondary weapon sheathed", function(assert) {
-    assert.ok(swlcalc.gear.slots.weapon.recap.el.weaponActive.is(":visible"), "primary active");
-    assert.ok(!swlcalc.gear.slots.weapon2.recap.el.weaponActive.is(":visible"), "secondary passive");
+        createShuffledBuild();
+    }
 });
 
 QUnit.test("should swap weapon from primary to secondary", function(assert) {
@@ -35,16 +27,16 @@ QUnit.test("should swap weapon from primary to secondary and back again", functi
 });
 
 QUnit.test("should swap weapons and affect summary accordingly", function(assert) {
-    swlcalc.gear.slots.weapon2.edit.setEquipmentRarity("mythic");
-    swlcalc.gear.slots.weapon2.edit.setGlyphId("critical-power");
-
-    assert.equal($("#stat-weapon-power").html(), "1832");
-    assert.equal($("#stat-critical-chance").html(), "13.8%");
-    assert.equal($("#stat-critical-power-percentage").html(), "90.7%");
-
     swlcalc.gear.slots.weapon2.recap.el.section.click();
 
-    assert.equal($("#stat-weapon-power").html(), "2830");
-    assert.equal($("#stat-critical-chance").html(), "13.3%");
-    assert.equal($("#stat-critical-power-percentage").html(), "93.6%");
+    assert.equal($("#stat-weapon-power").html(), "2780");
+    assert.equal($("#stat-combat-power").html(), "532.6");
+    assert.equal($("#stat-healing-power").html(), "107.7");
+    assert.equal($("#stat-critical-rating").html(), "+960");
+    assert.equal($("#stat-critical-chance").html(), "11.3%");
+    assert.equal($("#stat-critical-power-percentage").html(), "83.3%");
+    assert.equal($("#stat-protection").html(), "5850");
+    assert.equal($("#stat-damage-mitigation").html(), "19.3%");
+    assert.equal($("#stat-evade-rating").html(), "+1192");
+    assert.equal($("#stat-evade-chance").html(), "8.2%");
 });
